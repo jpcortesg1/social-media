4aca340f7ae04621937c4493b76835d7e523d511
jpcortesg1 <jpcortesg@eafit.edu.co>
2022-01-21 13:22:17 -0500

    new folders name

---
+++ client/src/components/rightbar/Rightbar.jsx
@@ -0,0 +1,109 @@
+import "./rightbar.css";
+import { Users } from "./../../dummyData";
+import Online from "../online/Online";
+
+export default function Rightbar({ profile }) {
+  const HomeRightbar = () => {
+    return (
+      <>
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="assets/gift.png" alt="" />
+          <span className="birthdayText">
+            <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today.
+          </span>
+        </div>
+        <img src="./assets/ad.png" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          {Users.map((user) => (
+            <Online key={user.id} user={user} />
+          ))}
+        </ul>
+      </>
+    );
+  };
+
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitleProfile">User Information</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rigthbarInfoKey">City: </span>
+            <span className="rigthbarInfoValue">New York</span>
+          </div>
+
+          <div className="rightbarInfoItem">
+            <span className="rigthbarInfoKey">From: </span>
+            <span className="rigthbarInfoValue">Madrid</span>
+          </div>
+
+          <div className="rightbarInfoItem">
+            <span className="rigthbarInfoKey">Relationship: </span>
+            <span className="rigthbarInfoValue">Single</span>
+          </div>
+        </div>
+        <h4 className="rightbarTitleProfile">User friends</h4>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <img
+              src="./assets/person/1.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Jhon Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="./assets/person/2.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Jhon Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="./assets/person/3.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Jhon Carter</span>
+          </div>
+
+          <div className="rightbarFollowing">
+            <img
+              src="./assets/person/4.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Jhon Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="./assets/person/5.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Jhon Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="./assets/person/6.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Jhon Carter</span>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        {profile ? <ProfileRightbar /> : <HomeRightbar />}
+      </div>
+    </div>
+  );
+}
