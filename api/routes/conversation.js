5244fb42dee1c04beae1acb8b549324f4297c58b
jpcortesg1 <jpcortesg@eafit.edu.co>
2022-02-26 01:35:22 -0500

    change in logic to follow and unfollow friend and add route to find a conversation with two id's

--- api/routes/conversation.js
+++ api/routes/conversation.js
@@ -4,7 +4,7 @@ const Conversation = require("../models/Conversation");
 // New conversation
 router.post("/", async (req, res) => {
   const newConversation = new Conversation({
-    members: [req.body.senderID, req.body.receiverID],
+    members: [req.body.senderId, req.body.receiverId],
   });
 
   try {
@@ -20,6 +20,18 @@ router.get("/:userId", async (req, res) => {
   try {
     const conversation = await Conversation.find({
       members: { $in: [req.params.userId] },
+    });
+    res.status(200).json(conversation);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Get conversation that includes two userId
+router.get("/find/:firstUserId/:secondUserId", async (req, res) => {
+  try {
+    const conversation = await Conversation.findOne({
+      members: { $all: [req.params.firstUserId, req.params.secondUserId] },
     });
     res.status(200).json(conversation);
   } catch (error) {
--- api/routes/users.js
+++ api/routes/users.js
@@ -86,7 +86,11 @@ router.put("/:id/follow", async (req, res) => {
     try {
       const user = await User.findById(req.params.id);
       const currentUser = await User.findById(req.body.userId);
-      if (!user.followers.includes(req.body.userId)) {
+      // if (!user.followers.includes(req.body.userId)) {
+      if (
+        !currentUser.followings.includes(req.params.id) &&
+        !user.followers.includes(req.body.userId)
+      ) {
         await user.updateOne({ $push: { followers: req.body.userId } });
         await currentUser.updateOne({ $push: { followings: req.params.id } });
         res.status(200).json("user has been followed");
@@ -107,7 +111,10 @@ router.put("/:id/unfollow", async (req, res) => {
     try {
       const user = await User.findById(req.params.id);
       const currentUser = await User.findById(req.body.userId);
-      if (user.followers.includes(req.body.userId)) {
+      if (
+        currentUser.followings.includes(req.params.id) ||
+        user.followers.includes(req.body.userId)
+      ) {
         await user.updateOne({ $pull: { followers: req.body.userId } });
         await currentUser.updateOne({ $pull: { followings: req.params.id } });
         res.status(200).json("user has been unfollowed");
